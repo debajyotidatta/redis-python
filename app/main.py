a72bfa60a600a33fdf9ab98f74aa8c6eabc933a9
Debajyoti Datta <datta24421112@gmail.com>
2023-03-11 17:46:28 -0800

    test

--- app/main.py
+++ app/main.py
@@ -10,6 +10,28 @@ def process_resp_string(resp_string):
             if j[0]!= '*' and j[0]!= '$':
                 new_str.append(j.rstrip('\r'))
     return " ".join(new_str[1:])
+
+def process_get(resp_string, result_dict):
+    resp_string = resp_string.split("\n")
+    new_str = []
+    for j in resp_string:
+        if len(j)>0:
+            if j[0]!= '*' and j[0]!= '$':
+                new_str.append(j.rstrip('\r'))
+    if new_str[1] in result_dict:
+        return result_dict[new_str[1]]
+    else:
+        return "nil"
+
+def process_set(resp_string, result_dict):
+    resp_string = resp_string.split("\n")
+    new_str = []
+    for j in resp_string:
+        if len(j)>0:
+            if j[0]!= '*' and j[0]!= '$':
+                new_str.append(j.rstrip('\r'))
+    result_dict[new_str[1]] = new_str[2]
+    return "OK"
         
 
 def handle_client(conn, addr):
@@ -19,17 +41,20 @@ def handle_client(conn, addr):
         data = conn.recv(6379)
         if data.decode() == "QUIT":
             connected = False
-        elif ("ECHO" in data) or ("echo" in data):
-            output_string = process_resp_string(data.decode())
+        elif ("ECHO" in data.decode()) or ("echo" in data.decode()):
+            output_string = data.decode()
+            output_string = process_resp_string(output_string)
             conn.sendall(b"+%s\r\n" % output_string.encode())
-        # elif ("ECHO" in data.decode()) or ("echo" in data.decode()):
-        #     output_string = data.decode()
-        #     output_string = process_resp_string(output_string)
-        #     conn.sendall(b"+%s\r\n" % output_string.encode())
-        # elif ("ECHO" in data.decode()) or ("echo" in data.decode()):
-        #     output_string = data.decode()
-        #     output_string = process_resp_string(output_string)
-        #     conn.sendall(b"+%s\r\n" % output_string.encode())
+        elif ("GET" in data.decode()) or ("get" in data.decode()):
+            output_string = data.decode()
+            output_string = process_get(output_string, result_dict)
+            # print(result_dict)
+            conn.sendall(b"+%s\r\n" % output_string.encode())
+        elif ("SET" in data.decode()) or ("set" in data.decode()):
+            output_string = data.decode()
+            output_string = process_set(output_string, result_dict)
+            # print(result_dict)
+            conn.sendall(b"+%s\r\n" % output_string.encode())
         elif data:
             # print(data)
             conn.sendall(b"+PONG\r\n")
